65b809e587c17c694435aa71a18ec6a6f3e2954a
AliHasanRazaKHan <alihsnrazakhan@gmail.com>
2024-02-10 01:32:05 +0500

    [MAIN]: tasks table creation

--- basic-mysql.sql
+++ basic-mysql.sql
@@ -1,1 +1,5 @@
 CREATE DATABASE training;
+USE training;
+CREATE TABLE tasks(entity_id INT NOT NULL,task_name VARCHAR(255) NOT NULL,
+description VARCHAR(255) NOT NULL,estimate_hours INT NOT NULL,
+assignee_name INT NOT NULL,status BOOLEAN NOT NULL DEFAULT 0,PRIMARY KEY(entity_id));

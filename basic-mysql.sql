8c7698e4775f1aa0565e4e812bdcc38f300c5653
AliHasanRazaKHan <alihsnrazakhan@gmail.com>
2024-02-10 01:35:38 +0500

    [MAIN]: estimate_hours alter table

--- basic-mysql.sql
+++ basic-mysql.sql
@@ -4,3 +4,4 @@ CREATE TABLE tasks(entity_id INT NOT NULL,task_name VARCHAR(255) NOT NULL,
 description VARCHAR(255) NOT NULL,estimate_hours INT NOT NULL,
 assignee_name INT NOT NULL,status BOOLEAN NOT NULL DEFAULT 0,PRIMARY KEY(entity_id));
 ALTER TABLE tasks MODIFY COLUMN assignee_name VARCHAR(30);
+ALTER TABLE tasks MODIFY COLUMN estimate_hours VARCHAR(10);
